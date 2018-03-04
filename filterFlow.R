2f7ff5fcb76a61bb8143b2e2b1989569881b1440
jknappe <jan.knappe@tcd.ie>
2018-03-04 15:28:01 +0000

    calculate residence time, assuming linearity and plug flow

---
+++ filterFlow.R
@@ -0,0 +1,221 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# filterFlow                                                           #
+# -----------                                                          #
+#                                                                      #
+# R script to calculate hydraulich retention time through a filter     #
+# author: Jan Knappe                                                   #
+# www.janknappe.com / jan.knappe@gmail.com                             #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+# LIBRARIES ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+library(tidyverse)
+library(readtext)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# CONSTANTS ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# .kFlowFactor ----
+# volume of water (in liters) per count
+kFlowFactor = 0.85 
+
+# .kInterval ----
+# interval bin width (in seconds)
+kInterval = 15 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+# DATA IMPORT ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# .rawData ----
+# read textfile created by "Stopwatch & Trainer (v1.1.7)" app for Android
+rawData = 
+  readtext(file = "data/filterFlow-180220.txt",
+           docvarsfrom = "filenames",
+           docvarnames = c("type", "date"),
+           dvsep = "-")
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+# DATA PROCESSING ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# .cumFlow ----
+# calculate cummulative flow from parsing lap times
+cumFlow = 
+  str_match_all(rawData[[2]], "Total Time - (.*?)\n\nLap")[[1]][, 2] %>%
+  as_tibble(.) %>%
+  mutate(hours = as.numeric(substring(value, 1, 2)),
+         mins = as.numeric(substring(value, 4, 5)),
+         secs = as.numeric(substring(value, 7, 8)),
+         mils = as.numeric(substring(value, 10, 12))) %>%
+  transmute(cumOutflowVolume.l = as.numeric(row.names(.)) * kFlowFactor,
+            elapsedTime.s = (mils + (1000 * secs) + (60000 * mins) + (3600000 * hours))/1000)
+
+# plot cummulative flow
+ ggplot(cumFlow, aes(x = elapsedTime.s, y = cumOutflowVolume.l)) + geom_line() +geom_point()
+
+# .intervalFlow ----
+# calculate outflow per interval bin
+intervalFlow = 
+  cumFlow %>%
+  mutate(intervalStartTime.s = kInterval * (round((elapsedTime.s + kInterval/2) / kInterval))) %>%
+  group_by(intervalStartTime.s) %>% 
+  summarize(cumOutflowVolume.l = max(cumOutflowVolume.l),
+            intervalOutflowRate.l_min = n() * kFlowFactor * 60/kInterval) 
+
+# .paddedFlow ----
+# pad flow data with intervals bins of no-flow conditions
+# and calculate storage and net flow
+paddedFlow = 
+  max(intervalFlow$intervalStartTime.s) %>%
+  seq(from = 0, to = ., by = kInterval) %>%
+  tibble(intervalStartTime.s = .) %>%
+  left_join(intervalFlow, by = "intervalStartTime.s") %>%
+  fill(cumOutflowVolume.l) %>%
+  transmute(intervalStartTime.min = intervalStartTime.s / 60,
+            intervalOutflowRate.l_min = ifelse(is.na(intervalOutflowRate.l_min), 0, intervalOutflowRate.l_min),
+            intervalInflowRate.l_min = ifelse(intervalStartTime.s < 300, sum(intervalOutflowRate.l_min)/((300/kInterval)), 0),
+            cumOutflowVolume.l = ifelse(is.na(cumOutflowVolume.l), 0, cumOutflowVolume.l),
+            cumInflowVolume.l = cumsum(intervalInflowRate.l_min) / (60/kInterval),
+            storedWaterVolume.l = cumInflowVolume.l - cumOutflowVolume.l,
+            intervalVolumeChange.l_min = intervalInflowRate.l_min - intervalOutflowRate.l_min)
+
+# .residenceTime ----
+# calculate residence time in water filter
+# assumption: water flow between count events is constant
+residenceTime = as_tibble(list(intervalStartTime.min = as.vector(rep(NA, times = length(paddedFlow$intervalStartTime.min))), 
+                               outflowEndTime.min = as.vector(rep(NA, times = length(paddedFlow$intervalStartTime.min)))))
+linearFlowTimeRatio = NA
+linearEndTime.min = NA
+
+for (i in 1:length(paddedFlow$intervalStartTime.min)) {
+  for (j in 2:length(paddedFlow$intervalStartTime.min)) {
+    # ratio between two time slices
+    linearFlowTimeRatio = 
+      ifelse (paddedFlow$cumInflowVolume.l[[i]] <= paddedFlow$cumOutflowVolume.l[[j]], 
+              break, 
+              (paddedFlow$cumInflowVolume.l[[i]] - paddedFlow$cumOutflowVolume.l[[j]]) / (paddedFlow$cumOutflowVolume.l[[j+1]] - paddedFlow$cumOutflowVolume.l[[j]]));
+    # linear time point between two slices
+    linearEndTime.min = 
+      linearFlowTimeRatio * (paddedFlow$intervalStartTime.min[[j+1]] - paddedFlow$intervalStartTime.min[[j]]) + paddedFlow$intervalStartTime.min[[j]];
+  }
+  residenceTime$intervalStartTime.min[[i]] = paddedFlow$intervalStartTime.min[[i]]
+  residenceTime$outflowEndTime.min[[i]] = linearEndTime.min
+}
+
+# .linearFlow ----
+# combine data
+linearFlow = 
+  paddedFlow %>%
+  left_join(residenceTime) %>%
+  mutate(outflowStartTime.min = lag(outflowEndTime.min, default = min(cumFlow$elapsedTime.s) / 60),
+         # remove observations without change in time intervals
+         cumOutflowVolume.l = ifelse(cumOutflowVolume.l == lag(cumOutflowVolume.l) & cumOutflowVolume.l != 0, NA, cumOutflowVolume.l),
+         storedWaterVolume.l = ifelse(storedWaterVolume.l == lag(storedWaterVolume.l) & intervalInflowRate.l_min == 0, NA, storedWaterVolume.l),
+         # remove obervations that don't make sense after inflow stopped
+         outflowStartTime.min = ifelse(intervalInflowRate.l_min == 0, NA, outflowStartTime.min),
+         outflowEndTime.min = ifelse(intervalInflowRate.l_min == 0, NA, outflowEndTime.min),
+         residenceTime.min = ifelse(intervalInflowRate.l_min == 0, NA, outflowStartTime.min - intervalStartTime.min))
+
+# .filterFlows ----
+# tidy data
+filterFlows = 
+  linearFlow %>%
+  select(intervalStartTime.min, 
+         intervalInflowRate.l_min, 
+         intervalOutflowRate.l_min, 
+         intervalVolumeChange.l_min) %>%
+  gather(key = flowType,
+         value = flowRate.l_min,
+         -intervalStartTime.min) %>%
+  filter(!is.na(flowRate.l_min)) %>%
+  mutate(flowType = case_when(flowType %in% "intervalInflowRate.l_min" ~ "interval inflow rate",
+                              flowType %in% "intervalOutflowRate.l_min" ~ "interval outflow rate",
+                              flowType %in% "intervalVolumeChange.l_min" ~ "interval stored volume change rate"),
+         flowType = factor(flowType))
+
+# .filterVolumes ----
+# tidy data 
+filterVolumes = 
+  linearFlow %>%
+  select(intervalStartTime.min, 
+         cumInflowVolume.l, 
+         cumOutflowVolume.l, 
+         storedWaterVolume.l) %>%
+  gather(key = volumeType,
+         value = waterVolume.l,
+         -intervalStartTime.min) %>%
+  filter(!is.na(waterVolume.l)) %>%
+  mutate(volumeType = case_when(volumeType %in% "cumInflowVolume.l" ~ "cummulative water inflow",
+                                volumeType %in% "cumOutflowVolume.l" ~ "cummulative water outflow",
+                                volumeType %in% "storedWaterVolume.l" ~ "stored water"),
+         volumeType = factor(volumeType))  
+
+# .filterTimes ----
+# tidy data 
+filterTimes = 
+  linearFlow %>%
+  select(intervalStartTime.min, 
+         outflowStartTime.min,
+         outflowEndTime.min,
+         residenceTime.min) %>%
+  gather(key = timeType,
+         value = elapsedTime.min,
+         -intervalStartTime.min) %>%
+  filter(!is.na(elapsedTime.min)) %>%
+  mutate(timeType = case_when(timeType %in% "outflowEndTime.min" ~ "outflow end",
+                              timeType %in% "outflowStartTime.min" ~ "outflow start",
+                              timeType %in% "residenceTime.min" ~ "residence time"),
+         timeType = factor(timeType))
+  
+# PLOTS ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# .filterFlows ----
+ggplot(data = filterFlows, aes(x = intervalStartTime.min, y = flowRate.l_min, color = flowType)) + 
+  #geom_line(size = 1.2) + 
+  geom_point(size = 2.5, shape = 21, fill = "white") +
+  geom_step(size = 1.2) +
+ # facet_wrap(~flowType, nrow = 1) +
+  theme_light() + 
+  scale_color_manual(values=c( "#FF6655", "#77BBFF", "#66CC99", "#CCBB66")) +
+  labs(x = "elapsed Time [min]", y = expression(paste("water flow [L", " ", min^{-1}, "]"))) +
+  theme(text = element_text(size = 16, family = "serif"),
+        axis.title.x = element_text(margin = margin(t = 20, r = 0, b = 0, l = 0)),
+        axis.title.y = element_text(margin = margin(t = 0, r = 15, b = 0, l = 0)),
+        legend.position = "bottom",
+        panel.background = element_rect(fill = "#F4F4F4")) 
+
+# .filterVolumes ----
+ggplot(data = filterVolumes, aes(x = intervalStartTime.min, y = waterVolume.l, color = volumeType)) + 
+  geom_line(size = 1.2) + 
+  geom_point(size = 2.5, shape = 21, fill = "white") +
+  #geom_step(size = 1.2) +
+  # facet_wrap(~flowType, nrow = 1) +
+  theme_light() + 
+  scale_color_manual(values=c( "#FF6655", "#77BBFF", "#66CC99", "#CCBB66")) +
+  labs(x = "elapsed Time [min]", y = expression(paste("water volume [L]"))) +
+  theme(text = element_text(size = 16, family = "serif"),
+        axis.title.x = element_text(margin = margin(t = 20, r = 0, b = 0, l = 0)),
+        axis.title.y = element_text(margin = margin(t = 0, r = 15, b = 0, l = 0)),
+        legend.position = "bottom",
+        panel.background = element_rect(fill = "#F4F4F4")) 
+
+# .filterTimes ----
+ggplot(data = filterTimes, aes(x = intervalStartTime.min, y = elapsedTime.min, color = timeType)) + 
+  geom_line(size = 1.2) + 
+  geom_point(size = 2.5, shape = 21, fill = "white") +
+  #geom_step(size = 1.2) +
+  # facet_wrap(~flowType, nrow = 1) +
+  theme_light() + 
+  scale_color_manual(values=c( "#FF6655", "#77BBFF", "#66CC99", "#CCBB66")) +
+  labs(x = "elapsed Time [min]", y = expression(paste("time [min]"))) +
+  theme(text = element_text(size = 16, family = "serif"),
+        axis.title.x = element_text(margin = margin(t = 20, r = 0, b = 0, l = 0)),
+        axis.title.y = element_text(margin = margin(t = 0, r = 15, b = 0, l = 0)),
+        legend.position = "bottom",
+        panel.background = element_rect(fill = "#F4F4F4")) 
+
